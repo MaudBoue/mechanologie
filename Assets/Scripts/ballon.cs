498d3c356d712652621022959b0f546e01ff6d04
EmilieBrsx <e.brusseaux@gmail.com>
2016-03-16 00:43:32 +0100

    Added files via upload

---
+++ Assets/Scripts/ballon.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballon : MonoBehaviour {
+
+    private new Rigidbody rigidbody;
+    private int force = 30;
+
+    //Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Update()
+    {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Debug.Log("touch");
+
+            float inputX = -Input.GetTouch(0).deltaPosition.x;
+            float inputZ = -Input.GetTouch(0).deltaPosition.y;
+
+                GetComponent<Rigidbody>().AddForce(Vector3.up * force * 2);
+                GetComponent<Rigidbody>().AddTorque(Vector3.right * 10);
+            { Debug.Log("roule"); }
+
+            }
+     
+    }
+
+    }
